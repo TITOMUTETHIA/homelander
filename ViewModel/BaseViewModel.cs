0b13c108dd5cd2ea76efc3ee43ab0d2ac9629851
Tito Mutethia <titomutethia@gmail.com>
2026-06-22 21:45:14 +0300

    view models complete

--- ViewModel/BaseViewModel.cs
+++ ViewModel/BaseViewModel.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.ComponentModel;
 using System.Text;
 
 namespace homeapp.ViewModel
 {
-    internal class BaseViewModel
+    public class BaseViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
---
+++ ViewModel/DetailsViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace homeapp.ViewModel
+{
+    internal class DetailsViewModel
+    {
+    }
+}
