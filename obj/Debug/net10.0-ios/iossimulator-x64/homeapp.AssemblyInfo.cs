945d42b69b5fcdf7384cf22b9108059197bbf611
Tito Mutethia <titomutethia@gmail.com>
2026-06-23 23:58:11 +0300

    Radio button fix

--- obj/Debug/net10.0-ios/iossimulator-x64/homeapp.AssemblyInfo.cs
+++ obj/Debug/net10.0-ios/iossimulator-x64/homeapp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("homeapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+9639f7354c0e66696798d46a7c3dac757785105e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+67d347eaccdb6fb874ae031d5c7ad1359ae5c03d")]
 [assembly: System.Reflection.AssemblyProductAttribute("homeapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("homeapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
