4bd6914478ec18ad33401dd3e8f78d9d2f465f26
Tito Mutethia <titomutethia@gmail.com>
2026-06-25 20:57:40 +0300

    Home model class

---
+++ Properties/Model/Home.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace homeapp.Properties.Model
+{
+    internal class Home
+    {
+    }
+}
--- obj/Debug/net10.0-windows10.0.19041.0/win-x64/homeapp.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows10.0.19041.0/win-x64/homeapp.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("homeapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+381ff8228f42bfc1965876d8a359e65ebb242a77")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+96f264f161b249b96245f1ff6187e24460e92c56")]
 [assembly: System.Reflection.AssemblyProductAttribute("homeapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("homeapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
