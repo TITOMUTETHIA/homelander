7b039555f89d53bf067215d4bc36347fb96fb3f9
Tito Mutethia <titomutethia@gmail.com>
2026-06-28 16:06:07 +0300

    logic for detais page

--- obj/Debug/net10.0-windows10.0.19041.0/win-x64/homeapp.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows10.0.19041.0/win-x64/homeapp.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("homeapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+6418babf8aa005ae681ad8128065f3cef0d54d2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+90ce6f85c32a6553aaf3802037444822764a5211")]
 [assembly: System.Reflection.AssemblyProductAttribute("homeapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("homeapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
