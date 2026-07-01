710016c950c5b1eb99699b9f46002f0dd43c1b65
Tito Mutethia <titomutethia@gmail.com>
2026-07-01 22:49:02 +0300

    new namespace using homeapp.Model

--- ViewModel/DetailsViewModel.cs
+++ ViewModel/DetailsViewModel.cs
@@ -3,18 +3,19 @@ using System.Threading.Tasks;
 using System.Collections.Generic;
 using System;
 using System.Text;
+using System.ComponentModel;
+using System.Windows.Input;
+
 namespace homeapp.ViewModel
 {
-    public class DetailsViewModel : BaseViewModel
+    public partial class DetailsViewModel : BaseViewModel, INotifyPropertyChanged
     {
-        public class DetailsViewModel : BaseViewModel
-        {
-          public Home SelectedProperty { get; set; }
-          public List<string> HomeImages { get; set; } = new List<string>();
-          public int MoreItems { get; set; }
+        public Home SelectedProperty { get; set; }
+        public List<string> HomeImages { get; set; } = new List<string>();
+        public int MoreItems { get; set; }
+        public ICommand CloeCommand => new Command(() => Application.Current.MainPage.Navigation.PopAsync()); 
 
-          public ICommand CloeCommand => new Command(() => Application.Current.MainPage.Navigation.PopAsync()); 
-         
+        // If you raise PropertyChanged in this class, use BaseViewModel.OnPropertyChanged or implement here.
+        public event PropertyChangedEventHandler? PropertyChanged;
     }
-}
 }
