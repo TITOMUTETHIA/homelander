26c446d4721ef42ab29838d17377dbee68feb0cd
Tito Mutethia <titomutethia@gmail.com>
2026-07-01 23:05:40 +0300

    seperate memebers from the base class using new key word

--- ViewModel/BaseViewModel.cs
+++ ViewModel/BaseViewModel.cs
@@ -1,10 +1,19 @@
 ﻿using System.Runtime.CompilerServices;
 using System.ComponentModel;
-
+using System.Windows.Input;
+using homeapp.View;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System;
+using homeapp.ViewModel;
+using System.Text;
+using homeapp.Model;
 namespace homeapp.ViewModel
 {
     public class BaseViewModel : INotifyPropertyChanged
     {
+        public ICommand CloeCommand => new Command(() => Application.Current.MainPage.Navigation.PopAsync());
         public event PropertyChangedEventHandler? PropertyChanged;
 
         public void OnPropertyChanged([CallerMemberName] string propertyName = null)
--- ViewModel/DetailsViewModel.cs
+++ ViewModel/DetailsViewModel.cs
@@ -5,17 +5,17 @@ using System;
 using System.Text;
 using System.ComponentModel;
 using System.Windows.Input;
+using homeapp.Model;
 
 namespace homeapp.ViewModel
 {
     public partial class DetailsViewModel : BaseViewModel, INotifyPropertyChanged
     {
-        public Home SelectedProperty { get; set; }
+        public required Home SelectedProperty { get; set; }
         public List<string> HomeImages { get; set; } = new List<string>();
         public int MoreItems { get; set; }
-        public ICommand CloeCommand => new Command(() => Application.Current.MainPage.Navigation.PopAsync()); 
 
         // If you raise PropertyChanged in this class, use BaseViewModel.OnPropertyChanged or implement here.
-        public event PropertyChangedEventHandler? PropertyChanged;
+        public new event PropertyChangedEventHandler? PropertyChanged;
     }
 }
