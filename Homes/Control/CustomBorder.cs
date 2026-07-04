e21e021544e47b9b72d074b91bd9ee3faa62e4d5
Tito Mutethia <titomutethia@gmail.com>
2026-07-04 23:16:44 +0300

    grid end tag fix

--- Homes/Control/CustomBorder.cs
+++ Homes/Control/CustomBorder.cs
@@ -1,8 +1,8 @@
-﻿using System;
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-using Microsoft.Maui;
-using Microsoft.Maui.Controls;
 
 namespace homeapp.Homes.Control
 {
