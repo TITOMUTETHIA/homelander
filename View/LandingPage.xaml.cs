7e6c3fd62f598deb8fafbb3b45fbfb5a8c626fa7
Tito Mutethia <titomutethia@gmail.com>
2026-07-08 21:11:35 +0300

    landing page class namespace

--- View/LandingPage.xaml.cs
+++ View/LandingPage.xaml.cs
@@ -2,7 +2,22 @@ using HomeApp.View;
 namespace HomeApp.View;
 
 
+public partial class using HomeApp.View;
+using homeapp.ViewModel;
+
+namespace HomeApp.View;
+
 public partial class LandingPage : ContentPage
+{
+    public LandingPage()
+    {
+        InitializeComponent();
+        this.BindingContext = new LandingViewModel();
+        // Set first radio button as checked if SectionList has items
+        if (SectionList.Children.Count > 0)
+            (SectionList.Children[0] as RadioButton).IsChecked = true;
+    }
+}LandingPage : ContentPage
 {
     public LandingPage()
     {
